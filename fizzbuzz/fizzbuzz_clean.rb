2156c1ac37f13f2e738c388b4f8d8363b1a44588
McTano <tsmackinlay@gmail.com>
2016-05-17 11:12:39 -0700

    Update fizzbuzz_clean.rb
    added comment

--- fizzbuzz/fizzbuzz_clean.rb
+++ fizzbuzz/fizzbuzz_clean.rb
@@ -1,3 +1,9 @@
+##
+# This is the result of my monkeying around with fizzbuzz
+# to find a more elegant solution than the first one I came
+# up with. My experiments are over at
+# https://github.com/McTano/fizzbuzz-solutions
+
 def fizzbuzzify(x)
   fizz = (x % 3 == 0) ? "Fizz" : nil 
   buzz = (x % 5 == 0) ? "Buzz" : nil
